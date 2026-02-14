3ac7570b6b4078caa0808be6002df30fcf6c0c4b
cesiumstaar <aryan1210katiyar@gmail.com>
2026-02-14 20:09:18 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -119,13 +119,13 @@ def main() -> None:
     parser.add_argument(
         "--vocab_word2idx",
         type=str,
-        default="/Users/apple/Documents/CS728/vocab_word2idx.json",
+        default="vocab_word2idx.json",
         help="Existing vocab word->idx JSON path",
     )
     parser.add_argument(
         "--vocab_idx2word",
         type=str,
-        default="/Users/apple/Documents/CS728/vocab_idx2word.json",
+        default="vocab_idx2word.json",
         help="Existing vocab idx->word JSON path",
     )
     parser.add_argument("--dims", type=str, default="50,100,200,300", help="Comma-separated dimensions")
